43dd5dc957b13244e4645dd75a12c7df3f16901f
nft-admin <protocolntfbox@gmail.com>
2021-05-18 19:38:18 +0600

    First init NFTBOX Application on IOS

---
+++ NFTBOX/NFTBOX/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NFTBOX
+//
+//  Created by NFTBOX Corporation
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
